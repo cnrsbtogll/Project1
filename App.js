a2e092d63931929ff24d93b95ccea492f018e322
cnrsbtogll <cnrsbtogll@gmail.com>
2024-01-22 00:09:31 +0300

    react navigation added

--- App.js
+++ App.js
@@ -1,81 +1,22 @@
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import {
-  Pressable,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  Image,
-} from "react-native";
-import Loading from "./src/componets/Loading";
+import { Text, View } from 'react-native'
+import React from 'react'
+import LoginScreen from './src/screens/LoginScreen'
+import {NavigationContainer} from '@react-navigation/native';
+import SignupScreen from './src/screens/SignupScreen';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-export default function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
+const Stack = createNativeStackNavigator();
 
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Image
-        source={require("./assets/images/login-icon.png")}
-        style={styles.login_icon}
-      />
-      <Text>Email</Text>
-      <TextInput
-        style={styles.textInputStyle}
-        onChangeText={setEmail}
-        keyboardType="email-address"
-        autoCapitalize="none"
-      />
-      <Text>Password</Text>
-      <TextInput
-        style={styles.textInputStyle}
-        onChangeText={setPassword}
-        secureTextEntry
-        keyboardType="number-pad"
-      />
-      <Pressable
-        style={({ pressed }) => [
-          styles.button,
-          { backgroundColor: pressed ? "lightgray" : "blue" },
-        ]}
-        onPress={() => setLoading(true)}
-      >
-        <Text style={{ color: "white" }}>Save</Text>
-      </Pressable>
-      <Loading visible={loading} onClose={() => setLoading(false)} />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  login_icon: {
-    width: 150,
-    height: 150,
-    marginVertical: 20,
-  },
+export default App
 
-  textInputStyle: {
-    height: 50,
-    width: "80%",
-    borderWidth: 1,
-    borderRadius: 10,
-    textAlign: "center",
-    marginVertical: 10,
-  },
-  button: {
-    height: 50,
-    width: "80%",
-    backgroundColor: "blue",
-    borderRadius: 10,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const SignupScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>SignupScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default SignupScreen
+
+const styles = StyleSheet.create({})
