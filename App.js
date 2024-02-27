41db95b0cce2730204219f7ea7c68e88536e911f
cnrsbtogll <cnrsbtogll@gmail.com>
2024-02-27 12:45:21 +0300

    yellowbox, root nav

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import RootNavigator from "./src/navigation/RootNavigator";
 import { Provider } from "react-redux";
 import { store } from "./src/redux/store";
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+LogBox.ignoreAllLogs();//Ignore all log notifications
 
 const Stack = createNativeStackNavigator();
 
--- src/navigation/RootNavigator.js
+++ src/navigation/RootNavigator.js
@@ -11,7 +11,7 @@ export default function RootNavigator() {
 
   return (
     <NavigationContainer>
-      {true ? <UserStack /> : <AuthStack />}
+      {isAuth ? <UserStack /> : <AuthStack />}
     </NavigationContainer>
   );
 }
